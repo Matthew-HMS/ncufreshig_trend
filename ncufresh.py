05e685d0790a32b19406129bf56cc0c2b3ff6250
Skywalker <imsho0218@gmail.com>
2023-09-05 20:13:27 +0800

    still can't use

--- ncufresh.py
+++ ncufresh.py
@@ -49,13 +49,10 @@ for j in range(name_list.__len__()):
         time.sleep(5)
         driver.get(link[index])
         time.sleep(3)
-        try:
-            # report = driver.find_element(By.XPATH, "//*[text()='查看洞察報告' or text()='View Insights']")
-            report = driver.find_element(By.XPATH, "//*[@class='x1i10hfl xjqpnuy xa49m3k xqeqjp1 x2hbi6w xdl72j9 x2lah0s xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r x2lwn1j xeuugli x1hl2dhg xggy1nq x1ja2u2z x1t137rt x1q0g3np x1lku1pv x1a2a7pz x6s0dn4 xjyslct x1ejq31n xd10rxx x1sy0etr x17r0tee x9f619 x1ypdohk x1i0vuye x1f6kntn xwhw2v2 xl56j7k x17ydfre x2b8uid xlyipyv x87ps6o x14atkfc xcdnw81 xjbqb8w xm3z3ea x1x8b98j x131883w x16mih1h x972fbf xcfux6l x1qhh985 xm0m39n xt0psk2 xt7dq6l xexx8yu x4uap5 x18d9i69 xkhd6sd x1n2onr6 x1n5bzlp x173jzuc x1yc6y37']")
-            report.click()
-            time.sleep(5)
-        except NoSuchElementException:
-            pass
+        # report = driver.find_element(By.XPATH, "//*[text()='查看洞察報告' or text()='View Insights']")
+        report = driver.find_element(By.XPATH, "//*[@class='x1i10hfl xjqpnuy xa49m3k xqeqjp1 x2hbi6w xdl72j9 x2lah0s xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r x2lwn1j xeuugli x1hl2dhg xggy1nq x1ja2u2z x1t137rt x1q0g3np x1lku1pv x1a2a7pz x6s0dn4 xjyslct x1ejq31n xd10rxx x1sy0etr x17r0tee x9f619 x1ypdohk x1i0vuye x1f6kntn xwhw2v2 xl56j7k x17ydfre x2b8uid xlyipyv x87ps6o x14atkfc xcdnw81 xjbqb8w xm3z3ea x1x8b98j x131883w x16mih1h x972fbf xcfux6l x1qhh985 xm0m39n xt0psk2 xt7dq6l xexx8yu x4uap5 x18d9i69 xkhd6sd x1n2onr6 x1n5bzlp x173jzuc x1yc6y37']")
+        report.click()
+        time.sleep(5)
 
         stats = driver.find_elements(By.XPATH, "//span[@data-bloks-name='bk.components.Text']")
         likes = int(stats[0].text.replace(',', ''))
